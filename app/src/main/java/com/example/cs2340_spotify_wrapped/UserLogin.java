5bddd7778dbd50347d10b270e8439ae7f1b52329
hartejsingh12 <12.singh.hartej@gmail.com>
2024-03-28 18:15:42 -0400

    Connected all the screens and xml files

--- app/src/main/java/com/example/cs2340_spotify_wrapped/UserLogin.java
+++ app/src/main/java/com/example/cs2340_spotify_wrapped/UserLogin.java
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -47,7 +46,7 @@ public class UserLogin extends AppCompatActivity {
         buttonLogin = findViewById(R.id.login_btn);
         mAuth = FirebaseAuth.getInstance();
         progressBar = findViewById(R.id.progressBar);
-        textview = findViewById(R.id.registerText);
+        textview = findViewById(R.id.register_text_view);
         textview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -94,5 +93,7 @@ public class UserLogin extends AppCompatActivity {
                         });
             }
         });
+
+
     }
 }
