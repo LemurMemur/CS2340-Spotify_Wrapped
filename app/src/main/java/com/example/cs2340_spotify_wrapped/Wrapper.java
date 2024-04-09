9e6cf2584705dacf41bffdd2846afb61cefb53ba
LemurMemur <37851353+LemurMemur@users.noreply.github.com>
2024-04-09 14:22:59 -0400

    song list and time selection working

--- app/src/main/java/com/example/cs2340_spotify_wrapped/Wrapper.java
+++ app/src/main/java/com/example/cs2340_spotify_wrapped/Wrapper.java
@@ -2,11 +2,16 @@ package com.example.cs2340_spotify_wrapped;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,15 +20,28 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class Wrapper extends AppCompatActivity {
+public class Wrapper extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    private String[] urls = {"https://api.spotify.com/v1/me/top/artists?limit=3"};
+    private String[] urls = {
+            "https://api.spotify.com/v1/me/top/artists?limit=5",
+            "https://api.spotify.com/v1/me/top/tracks?limit=5"
+    };
+    private String[] timeFrames = {
+            "&time_range=short_term",
+            "&time_range=medium_term",
+            "&time_range=long_term"
+    };
+
+    private Spinner timeSelectSpinner;
+    private static final String[] paths = {"1 Month", "6 Months", "12 Months"};
+    private static int currentTimeFrame = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +49,9 @@ public class Wrapper extends AppCompatActivity {
         setContentView(R.layout.activity_wrapper);
 
         initButtons();
+        initTimeSelect();
         initList(0);
-
+        initList(1);
     }
 
     private void initButtons() {
@@ -50,9 +69,37 @@ public class Wrapper extends AppCompatActivity {
         });
     }
 
-    private void initList (int mode) {
+    private void initTimeSelect() {
+        timeSelectSpinner = (Spinner)findViewById(R.id.timeSelectSpinner);
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>(
+                Wrapper.this,
+                android.R.layout.simple_spinner_item,
+                paths
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        timeSelectSpinner.setAdapter(adapter);
+        timeSelectSpinner.setOnItemSelectedListener(this);
+        timeSelectSpinner.setSelection(currentTimeFrame);
+    }
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item is selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos).
+        if (pos != currentTimeFrame) {
+            currentTimeFrame = pos;
+            finish();
+            startActivity(getIntent());
+        }
+        ((TextView) parent.getChildAt(0)).setTextColor(Color.WHITE);
+    }
 
-        JSONObject ret = null;
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback.
+    }
+
+
+
+    private void initList (int mode) {
         if (MainActivity.mAccessToken == null) {
             Toast.makeText(this, "You need to get an access token first!", Toast.LENGTH_SHORT).show();
             return;
@@ -60,13 +107,10 @@ public class Wrapper extends AppCompatActivity {
 
         // Create a request to get the user profile
         final Request request = new Request.Builder()
-                .url(urls[mode])
+                .url(urls[mode] + timeFrames[currentTimeFrame])
                 .addHeader("Authorization", "Bearer " + MainActivity.mAccessToken)
                 .build();
-
-
         Call mCall = MainActivity.mOkHttpClient.newCall(request);
-
         mCall.enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
@@ -90,85 +134,54 @@ public class Wrapper extends AppCompatActivity {
     }
 
     private void fillText(JSONObject jo, int mode) {
-        try {
-            switch (mode) {
-                case 0: // artist
-                    System.out.println(jo);
-                    JSONArray items = jo.getJSONArray("items");
-                    LinearLayout artistList = findViewById(R.id.topArtist_list);
-                    for (int i = 0; i < 3 && i < items.length(); i++) {
-                        JSONObject item = items.getJSONObject(i);
-                        int finalI = i;
-                        try {
-                            ((TextView) artistList.getChildAt(finalI)).setText(item.getString("name"));
-                        } catch (JSONException e) {
-                            throw new RuntimeException(e);
+        runOnUiThread(() -> {
+            try {
+                JSONArray items = jo.getJSONArray("items");
+                switch (mode) {
+                    case 0: // artist
+                        System.out.println(jo);
+                        LinearLayout artistList = findViewById(R.id.topArtist_list);
+                        HashMap<String, Integer> genreList;
+                        for (int i = 0; i < 3; i++) {
+                            String artist = "";
+                            if (i < items.length()) {
+                                JSONObject item = items.getJSONObject(i);
+                                System.out.println(item.getString("name"));
+                                artist = item.getString("name");
+                            }
+                            try {
+                                ((TextView) artistList.getChildAt(i)).setText(artist);
+                            } catch (Exception e) {}
                         }
-                        System.out.println(item.getString("name"));
-                    }
-                    break;
-                default:
-            }
+                        for (int i = 0; i < items.length(); i++) {
+                            JSONObject item = items.getJSONObject(i);
+                            System.out.println(item.getString("name"));
+                        }
+                        break;
+                    case 1: // songs
+                        System.out.println(jo);
+                        LinearLayout songList = findViewById(R.id.topSong_list);
+                        for (int i = 0; i < 3; i++) {
+                            String song = "";
+                            if (i < items.length()) {
+                                JSONObject item = items.getJSONObject(i);
+                                System.out.println(item.getString("name"));
+                                song = item.getString("name");
+                            }
+                            try {
+                                ((TextView) songList.getChildAt(i)).setText(song);
+                            } catch (Exception e) {}
+                        }
+                        break;
 
-        } catch (JSONException e) {
-            Log.d("JSON", "Failed to parse data: " + e);
-            Toast.makeText(Wrapper.this, "Failed to parse data, watch Logcat for more details",
-                    Toast.LENGTH_SHORT).show();
-        }
-    }
+                    default:
+                }
 
-    private void initSongsList() {
-        if (MainActivity.mAccessToken == null) {
-            Toast.makeText(this, "You need to get an access token first!", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        // Create a request to get the user profile
-        final Request request = new Request.Builder()
-                .url("https://api.spotify.com/v1/me/top/tracks?limit=3")
-                .addHeader("Authorization", "Bearer " + MainActivity.mAccessToken)
-                .build();
-
-
-        Call mCall = MainActivity.mOkHttpClient.newCall(request);
-
-        mCall.enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                Log.d("HTTP", "Failed to fetch data: " + e);
-                Toast.makeText(Wrapper.this, "Failed to fetch data, watch Logcat for more details",
+            } catch (JSONException e) {
+                Log.d("JSON", "Failed to parse data: " + e);
+                Toast.makeText(Wrapper.this, "Failed to parse data, watch Logcat for more details",
                         Toast.LENGTH_SHORT).show();
             }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                try {
-                    final JSONObject jsonObject = new JSONObject(response.body().string());
-                    fillSongsText(jsonObject);
-                } catch (JSONException e) {
-                    Log.d("JSON", "Failed to parse data: " + e);
-                    Toast.makeText(Wrapper.this, "Failed to parse data, watch Logcat for more details",
-                            Toast.LENGTH_SHORT).show();
-                }
-            }
         });
-    }
-
-    private void fillSongsText(JSONObject jo) {
-        try {
-            JSONArray items = jo.getJSONArray("items");
-            LinearLayout artistList = findViewById(R.id.topSong_list);
-            for (int i = 0; i < 3 && i < items.length(); i++) {
-                JSONObject item = items.getJSONObject(i);
-                int finalI = i;
-                runOnUiThread(() -> ((TextView)artistList.getChildAt(finalI)).setText(item.toString()));
-                System.out.println(item.toString());
-            }
-
-        } catch (JSONException e) {
-            Log.d("JSON", "Failed to parse data: " + e);
-            Toast.makeText(Wrapper.this, "Failed to parse data, watch Logcat for more details",
-                    Toast.LENGTH_SHORT).show();
-        }
     }
 }
