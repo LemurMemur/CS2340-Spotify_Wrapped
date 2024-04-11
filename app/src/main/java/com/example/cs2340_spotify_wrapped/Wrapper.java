2767485065adb895299cff5544ba1660d0200f47
hartejsingh12 <12.singh.hartej@gmail.com>
2024-04-11 01:55:51 -0400

    fixed functionality

--- app/src/main/java/com/example/cs2340_spotify_wrapped/Wrapper.java
+++ app/src/main/java/com/example/cs2340_spotify_wrapped/Wrapper.java
@@ -2,7 +2,6 @@ package com.example.cs2340_spotify_wrapped;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
@@ -42,13 +41,9 @@ public class Wrapper extends AppCompatActivity implements AdapterView.OnItemSele
     };
 
     private Spinner timeSelectSpinner;
-
-    private ImageButton setting;
     private static final String[] paths = {"1 Month", "6 Months", "12 Months"};
     private static int currentTimeFrame = 1;
     private static WrapperData currWrapperData;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,13 +51,10 @@ public class Wrapper extends AppCompatActivity implements AdapterView.OnItemSele
         setContentView(R.layout.activity_wrapper);
 
         currWrapperData = new WrapperData();
-        setting = findViewById(R.id.setting);
         initButtons();
         initTimeSelect();
         initList(0);
         initList(1);
-
-
     }
 
     private void initButtons() {
@@ -78,8 +70,6 @@ public class Wrapper extends AppCompatActivity implements AdapterView.OnItemSele
         profile.setOnClickListener((v) -> {
             //TODO add redirect to recommendation page
         });
-
-
     }
 
     private void initTimeSelect() {
