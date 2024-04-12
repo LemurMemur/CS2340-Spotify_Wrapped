a87bf0fa926fcea6d5ff586a193827c3d73d67e1
chanhsulin <CHAN49769094@GMAIL.COM>
2024-04-11 21:02:53 -0400

    added background animation to both homePage and wrapper

--- app/src/main/java/com/example/cs2340_spotify_wrapped/HomePage.java
+++ app/src/main/java/com/example/cs2340_spotify_wrapped/HomePage.java
@@ -2,12 +2,16 @@ package com.example.cs2340_spotify_wrapped;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class HomePage extends AppCompatActivity {
+    RelativeLayout home;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
@@ -41,5 +45,10 @@ public class HomePage extends AppCompatActivity {
             }
         });
 
+        home = findViewById(R.id.home);
+        AnimationDrawable animationDrawable = (AnimationDrawable) home.getBackground();
+        animationDrawable.setEnterFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(5000);
+        animationDrawable.start();
     }
 }
